db5304a6a60e4fe7fa190e5a266480a900d8c769
level09 <level09@gmail.com>
2024-12-01 13:58:15 +0100

    updated to flit-core

--- setup.py
+++ setup.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='Flask-uWSGI-WebSocket',
-    version='0.6.1',
-    url='https://github.com/zeekay/flask-uwsgi-websocket',
-    license='MIT',
-    author='Zach Kelling',
-    author_email='zk@monoid.io',
-    description='High-performance WebSockets for your Flask apps powered by uWSGI.',
-    long_description=open('README.rst').read(),
-    py_modules=['flask_uwsgi_websocket'],
-    zip_safe=False,
-    include_package_data=True,
+    name="flask-uwsgi-ws",
+    version="0.1.0",
+    description="Flask WebSocket extension for uWSGI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Nidal Alhariri",
+    author_email="leve09@gmail.com",
+    url="https://github.com/level09/flask-uwsgi-ws",
     packages=find_packages(),
-    platforms='any',
     install_requires=[
-        'Flask',
-        'uwsgi',
+        "flask>=2.0.0",
+        "uwsgi>=2.0.0",
     ],
+    python_requires=">=3.7",
     classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Flask",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
-    keywords='uwsgi flask websockets'
+    keywords="flask uwsgi websocket websockets ws async",
 )
